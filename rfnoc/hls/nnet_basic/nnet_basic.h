e2350f1c951b481c5736cd57511a0d895be05e44
EJ <ej@he360.com>
2017-04-22 14:58:19 -0400

    WIP: Read from file for data input from udacity notebook

--- rfnoc/hls/nnet_basic/nnet_basic.h
+++ rfnoc/hls/nnet_basic/nnet_basic.h
@@ -4,31 +4,34 @@
 #include <complex>
 #include "ap_int.h"
 
-#define N  11
+// TODO: convert data types to fixed point (bleh)
+typedef short int data_t;
 
-typedef short int input_t;
-typedef short int coeff_t;
-typedef short int bias_t;
-typedef short int result_t;
+typedef data_t input_t;
+typedef data_t coeff_t;
+typedef data_t bias_t;
+typedef data_t result_t;
 
+// TODO: Figure out if we're using axi-stream interfaces?
 struct axis {
     input_t data;
     ap_uint<1> last;
 };
 
+#ifndef N_LAYER_IN
+	#define N_LAYER_IN  3
+#endif
 
-#define N_LAYER_IN  3
-#define N_LAYER_OUT 3
+#ifndef N_LAYER_OUT
+	#define N_LAYER_OUT 3
+#endif
 
 // Prototype of top level function for C-synthesis
-//  TODO: Add coefficients
 void nnet_basic(
 	  input_t data[N_LAYER_IN],
 	  coeff_t weights[N_LAYER_IN][N_LAYER_OUT],
 	  bias_t  biases[N_LAYER_OUT],
 	  result_t res[N_LAYER_OUT]);
-//void nnet_basic(axis &in,
-//		        axis &out);
 
 void nnet_layer(
 	  input_t data[N_LAYER_IN],
--- rfnoc/hls/nnet_basic/nnet_basic_test.cpp
+++ rfnoc/hls/nnet_basic/nnet_basic_test.cpp
@@ -1,31 +1,104 @@
 #include <stdio.h>
 #include <math.h>
+
+#define N_LAYER_IN 784
+#define N_LAYER_OUT 10
+
 #include "nnet_basic.h"
+
+
+// int main(int argc, char **argv)
+// {
+//   // Dummy data for now
+//   input_t data[N_LAYER_IN] = {0, 1, 1};
+//   coeff_t coeffs[N_LAYER_IN][N_LAYER_OUT] = {{1,  0,  0},
+//                                              {0,  2,  0}, 
+//                                              {0, 10, 20}};
+//   bias_t  biases[N_LAYER_OUT] = {1, 2, 3};
+//   short int answer[N_LAYER_OUT] = {1, 14, 23};
+
+//   // Run the basic neural net block
+//   result_t res[N_LAYER_OUT];
+//   nnet_basic(data, coeffs, biases, res);
+
+//   // Print result vector
+//   int err_cnt = 0;
+//   for (int ii = 0; ii < N_LAYER_OUT; ii++) {
+//     std::cout << " Expected: " << answer[ii] << "   Received: " << res[ii] << std::endl;
+//     if (res[ii] != answer[ii]) err_cnt++;
+//   }
+//   return err_cnt;
+// }
+
+
+int read_file_1D(const char * filename, data_t *data, unsigned int nsamps)
+{
+  FILE *fp;
+//  data_t datain[nsamps];
+
+  fp = fopen(filename, "r");
+  std::cout<< "Reading 1D file: " << fp << std::endl;
+  if (fp == 0) {
+    std::cout << "Failed to open file" << std::endl;
+    return 1;
+  }
+  // Read data from file
+  float newval;
+  for (int ii = 0; ii < nsamps; ii++){
+    if (fscanf(fp, "%f\n", &newval) != 0){
+      data[ii] = newval;
+    } else {
+      std::cout << "File failed to extract data" << std::endl;
+      return 2;
+    }
+  }
+  fclose(fp);
+  return 0;
+}
+
+int read_file_2D(const char * filename, data_t *data, unsigned int nrows, unsigned int ncols)
+{
+  FILE *fp;
+//  data_t datain[ncols][nrows];
+
+  fp = fopen(filename, "r");
+  std::cout<< "Reading 2D file: " << fp << std::endl;
+  if (fp == 0) {
+    std::cout << "Failed to open file" << std::endl;
+    return 1;
+  }
+  // Read data from file
+  float newval;
+  for (int ii = 0; ii < nrows; ii++) {
+    for (int jj = 0; jj < ncols; jj++){
+      if (fscanf(fp, "%f\n", &newval) != 0){
+    	std::cout<<"Reading " << ii << " " << jj << " " << newval << std::endl;
+        data[ii*ncols+jj] = newval;
+      } else {
+        std::cout << "File failed to extract data" << std::endl;
+        return 2;
+      }
+    }
+  }
+  fclose(fp);
+  return 0;
+}
 
 
 int main(int argc, char **argv)
 {
-  // Dummy data for now
-  input_t data[N_LAYER_IN] = {0, 1, 1};
-  coeff_t coeffs[N_LAYER_IN][N_LAYER_OUT] = {{1,  0,  0},
-                                             {0,  2,  0}, 
-                                             {0, 10, 20}};
-  bias_t  biases[N_LAYER_OUT] = {1, 2, 3};
-  short int answer[N_LAYER_OUT] = {1, 14, 23};
+  // DATA FROM UDACITY TENSORFLOW CLASS
+  // 1-Layer test
 
-  // Run the basic neural net block
-  result_t res[N_LAYER_OUT];
-  nnet_basic(data, coeffs, biases, res);
+  coeff_t coeffs[N_LAYER_IN*N_LAYER_OUT];
+  bias_t  biases[N_LAYER_OUT];
 
-  // Print result vector
-  int err_cnt = 0;
-  for (int ii = 0; ii < N_LAYER_OUT; ii++) {
-    std::cout << " Expected: " << answer[ii] << "   Received: " << res[ii] << std::endl;
-    if (res[ii] != answer[ii]) err_cnt++;
-  }
-  return err_cnt;
+  // Load data from file
+  int rval = 0;
+  rval = read_file_1D("mnist_layer1_biases_10x1.dat", biases, N_LAYER_OUT);
+  rval = read_file_2D("mnist_layer1_weights_784x10.dat", coeffs, N_LAYER_IN, N_LAYER_OUT);
+
 }
-
 
 
 //int main () {
@@ -42,24 +115,24 @@ int main(int argc, char **argv)
 //  fp=fopen("out.dat","w");
 //  // Stimulate with impulses
 //  for (i=0;i<=SAMPLES;i++) {
-//  	if (ramp_up == 1)
-//  		signal = signal + 1;
-//  	else
-//  		signal = signal - 1;
+//    if (ramp_up == 1)
+//      signal = signal + 1;
+//    else
+//      signal = signal - 1;
 //
-//  	// Populate input data
-//  	in_data.data = signal;
-//  	in_data.last = (i == SAMPLES);
+//    // Populate input data
+//    in_data.data = signal;
+//    in_data.last = (i == SAMPLES);
 //
-//	// Execute the function with latest input
-//  	nnet_basic(in_data, out_data);
+//  // Execute the function with latest input
+//    nnet_basic(in_data, out_data);
 //
 //   if ((ramp_up == 1) && (signal >= 75))
-//   	ramp_up = 0;
+//    ramp_up = 0;
 //   else if ((ramp_up == 0) && (signal <= -75))
-//   	ramp_up = 1;
+//    ramp_up = 1;
 //
-//		// Save the results.
+//    // Save the results.
 //     printf("In: %i    Out: %i   Last: %i \n", in_data.data, out_data.data, (int)out_data.last);
 //    // fprintf(fp,"%i %i %i\n",  out_data.data.real(), out_data.data.imag(), (int)out_data.last);
 //  }
@@ -68,14 +141,14 @@ int main(int argc, char **argv)
 // //  printf ("Comparing against output data \n");
 // //  if (system("diff -w out.dat out.gold.dat")) {
 //
-//	// fprintf(stdout, "*******************************************\n");
-//	// fprintf(stdout, "FAIL: Output DOES NOT match the golden output\n");
-//	// fprintf(stdout, "*******************************************\n");
+//  // fprintf(stdout, "*******************************************\n");
+//  // fprintf(stdout, "FAIL: Output DOES NOT match the golden output\n");
+//  // fprintf(stdout, "*******************************************\n");
 // //     return 1;
 // //  } else {
-//	// fprintf(stdout, "*******************************************\n");
-//	// fprintf(stdout, "PASS: The output matches the golden output!\n");
-//	// fprintf(stdout, "*******************************************\n");
+//  // fprintf(stdout, "*******************************************\n");
+//  // fprintf(stdout, "PASS: The output matches the golden output!\n");
+//  // fprintf(stdout, "*******************************************\n");
 // //     return 0;
 // //  }
 //}
