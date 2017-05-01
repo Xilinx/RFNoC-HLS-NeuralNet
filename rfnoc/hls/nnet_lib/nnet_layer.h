dd9913fa91daa4b978db14e9dc518adb1cc7bada
ejk <ejkreinar@gmail.com>
2017-04-30 21:26:07 -0400

    Working on large-layer optimizations

--- rfnoc/hls/nnet_lib/nnet_layer.h
+++ rfnoc/hls/nnet_lib/nnet_layer.h
@@ -6,11 +6,14 @@
 template<class data_T, class res_T, class weight_T, class bias_T, class acc_T>
 class nnet_layer {
     protected:
-		template<int N_IN, int N_OUT>
-		void compute_small_outputs(data_T data[N_IN], res_T res[N_OUT], weight_T weights[N_IN][N_OUT], bias_T biases[N_OUT]);
+        // *************************************************
+        //       Possible implementation options
+        // *************************************************
+        template<int N_IN, int N_OUT>
+        void compute_small_outputs(data_T data[N_IN], res_T res[N_OUT], weight_T weights[N_IN][N_OUT], bias_T biases[N_OUT]);
 
-//		template<int N_IN, int N_OUT>
-//		void compute_large_outputs(data_T data[N_IN], res_T res[N_OUT], weight_T weights[N_IN][N_OUT], bias_T biases[N_OUT]);
+        template<int N_IN, int N_OUT>
+        void compute_large_outputs(data_T data[N_IN], res_T res[N_OUT], weight_T weights[N_IN][N_OUT], bias_T biases[N_OUT]);
     private:
     public:
         nnet_layer() { /*Do Nothing (for now)*/};
@@ -21,53 +24,95 @@ class nnet_layer {
 
 template<class data_T, class res_T, class weight_T, class bias_T, class acc_T>
 template<int N_IN, int N_OUT>
-void nnet_layer<data_T, res_T, weight_T, bias_T, acc_T>::compute_small_outputs(
-	data_T    data[N_IN],
-	res_T     res[N_OUT],
-	weight_T  weights[N_IN][N_OUT],
-	bias_T    biases[N_OUT])
+void nnet_layer<data_T, res_T, weight_T, bias_T, acc_T>::compute(
+    data_T    data[N_IN],
+    res_T     res[N_OUT],
+    weight_T  weights[N_IN][N_OUT],
+    bias_T    biases[N_OUT])
 {
-	#pragma HLS INLINE
-
-	data_T data_cache;
-  	acc_T acc[N_OUT];
-
-	#pragma HLS INTERFACE ap_fifo port=data
-	#pragma HLS ARRAY_RESHAPE variable=weights complete dim=2
-	#pragma HLS INTERFACE ap_fifo port=res
-    #pragma HLS ARRAY_PARTITION variable=acc complete dim=1
-
-	Reset: for(int iacc = 0; iacc < N_OUT; iacc++)
-	#pragma HLS UNROLL
-		acc[iacc] = 0;
-
-  	NewInput: for(int ii = 0; ii < N_IN; ii++) {
-	#pragma HLS PIPELINE
-
-		Product: for(int jj = 0; jj < N_OUT; jj++) {
-		#pragma HLS UNROLL
-			if (jj == 0) data_cache = data[ii];
-			weight_T weight = weights[ii][jj];
-			acc[jj] += data_cache * weight;
-			//std::cout << "Multiplying: " << weight << " x " << data_cache[ii] << std::endl;
-		}
+    if (N_OUT >= 32) {
+        compute_large_outputs<N_IN, N_OUT>(data, res, weights, biases);
     }
-
-	Result: for(int ires = 0; ires < N_OUT; ires++)
-		res[ires] = acc[ires] + biases[ires];
+    else {
+        compute_small_outputs<N_IN, N_OUT>(data, res, weights, biases);
+    }
 }
 
 template<class data_T, class res_T, class weight_T, class bias_T, class acc_T>
 template<int N_IN, int N_OUT>
-void nnet_layer<data_T, res_T, weight_T, bias_T, acc_T>::compute(
-	data_T    data[N_IN],
-	res_T     res[N_OUT],
-	weight_T  weights[N_IN][N_OUT],
-	bias_T    biases[N_OUT])
+void nnet_layer<data_T, res_T, weight_T, bias_T, acc_T>::compute_small_outputs(
+    data_T    data[N_IN],
+    res_T     res[N_OUT],
+    weight_T  weights[N_IN][N_OUT],
+    bias_T    biases[N_OUT])
 {
-	if (N_OUT < 2048) {
-		compute_small_outputs<N_IN, N_OUT>(data, res, weights, biases);
-	}
+    #pragma HLS INLINE
+
+    data_T data_cache;
+    acc_T acc[N_OUT];
+
+    #pragma HLS INTERFACE ap_fifo port=data
+    #pragma HLS INTERFACE ap_fifo port=res
+	#pragma HLS ARRAY_RESHAPE variable=weights complete dim=2
+    #pragma HLS ARRAY_PARTITION variable=acc complete dim=1
+
+    Reset: for(int iacc = 0; iacc < N_OUT; iacc++)
+    #pragma HLS UNROLL
+        acc[iacc] = 0;
+
+    NewInput: for(int ii = 0; ii < N_IN; ii++) {
+    #pragma HLS PIPELINE
+
+        Product: for(int jj = 0; jj < N_OUT; jj++) {
+        #pragma HLS UNROLL
+            if (jj == 0) data_cache = data[ii];
+            weight_T weight = weights[ii][jj];
+            acc[jj] += data_cache * weight;
+            //std::cout << "Multiplying: " << weight << " x " << data_cache[ii] << std::endl;
+        }
+    }
+
+    Result: for(int ires = 0; ires < N_OUT; ires++)
+        res[ires] = acc[ires] + biases[ires];
 }
+
+template<class data_T, class res_T, class weight_T, class bias_T, class acc_T>
+template<int N_IN, int N_OUT>
+void nnet_layer<data_T, res_T, weight_T, bias_T, acc_T>::compute_large_outputs(
+    data_T    data[N_IN],
+    res_T     res[N_OUT],
+    weight_T  weights[N_IN][N_OUT],
+    bias_T    biases[N_OUT])
+{
+    #pragma HLS INLINE
+//	#define NNET_LAYER_FACTOR 16
+
+    data_T data_cache;
+    acc_T acc[N_OUT];
+
+    #pragma HLS INTERFACE ap_fifo port=data
+    #pragma HLS INTERFACE ap_fifo port=res
+    #pragma HLS ARRAY_RESHAPE variable=weights block factor=16 dim=2
+    #pragma HLS ARRAY_PARTITION variable=acc block factor=16 dim=1
+
+    Reset: for(int iacc = 0; iacc < N_OUT; iacc++)
+    #pragma HLS UNROLL
+        acc[iacc] = 0;
+
+    NewInput: for(int ii = 0; ii < N_IN; ii++) {
+//    #pragma HLS PIPELINE
+
+        Product: for(int jj = 0; jj < N_OUT; jj++) {
+        #pragma HLS UNROLL factor=16
+            if (jj == 0) data_cache = data[ii];
+            weight_T weight = weights[ii][jj];
+            acc[jj] += data_cache * weight;
+        }
+    }
+
+    Result: for(int ires = 0; ires < N_OUT; ires++)
+        res[ires] = acc[ires] + biases[ires];
+}
+
 
 #endif
