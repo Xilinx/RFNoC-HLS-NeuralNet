9d97809caef0d1d69ac0460866bb581b0a1742cb
ejk <ejkreinar@gmail.com>
2017-05-10 23:06:54 -0400

    Converted 1 layer to ROM weights and biasess so the RFNOC interface is a lot easier

--- rfnoc/hls/ex_1layer/ex_1layer.cpp
+++ rfnoc/hls/ex_1layer/ex_1layer.cpp
@@ -1,13 +1,16 @@
 #include "ex_1layer.h"
 #include "nnet_layer.h"
 
+//#include "data/mnist_layer1_biases_small_256x1.h"
+//#include "data/mnist_layer1_weights_small_784x256.h"
+#include "data/mnist_layer1_biases_10x1.h"
+#include "data/mnist_layer1_weights_784x10.h"
+
 // AXI-Stream port type is compatible with pointer, reference, & array input / ouputs only
 // See UG902 Vivado High Level Synthesis guide (2014.4) pg 157 Figure 1-49
 //void nnet_basic (axis &in, axis &out) {
 void ex_1layer(
 	  input_t   data[N_LAYER_IN],
-	  coeff_t   weights[N_LAYER_IN][N_LAYER_OUT],
-	  bias_t    biases[N_LAYER_OUT],
 	  result_t  res[N_LAYER_OUT])
 {
     // Remove ap ctrl ports (ap_start, ap_ready, ap_idle, etc) since we only use the AXI-Stream ports
--- rfnoc/hls/ex_1layer/ex_1layer_test.cpp
+++ rfnoc/hls/ex_1layer/ex_1layer_test.cpp
@@ -11,20 +11,24 @@ int main(int argc, char **argv)
   // 1-Layer test
 
   input_t  data[N_LAYER_IN];
-  coeff_t coeffs[N_LAYER_IN][N_LAYER_OUT];
+  coeff_t weights[N_LAYER_IN][N_LAYER_OUT];
   bias_t  biases[N_LAYER_OUT];
   float answer[N_LAYER_OUT];
 
   // Load data from file
   int rval = 0;
   rval = read_file_1D<input_t, N_LAYER_IN>("data/mnist_validation_data_784x1.dat", data);
-  rval = read_file_2D<coeff_t, N_LAYER_IN, N_LAYER_OUT>("data/mnist_layer1_weights_784x10.dat", coeffs);
-  rval = read_file_1D<bias_t, N_LAYER_OUT>("data/mnist_layer1_biases_10x1.dat", biases);
-  rval = read_file_1D<float, N_LAYER_OUT>("data/mnist_validation_output_10x1.dat", answer);
+    rval = read_file_1D<float, N_LAYER_OUT>("data/mnist_validation_output_10x1.dat", answer);
+//  rval = read_file_2D<coeff_t, N_LAYER_IN, N_LAYER_OUT>("data/mnist_layer1_weights_784x10.dat", weights);
+//  rval = read_file_1D<bias_t, N_LAYER_OUT>("data/mnist_layer1_biases_10x1.dat", biases);
+//  rval = read_file_1D<input_t, N_LAYER_IN>("data/validation_data_784x1.dat", data);
+//  rval = read_file_2D<coeff_t, N_LAYER_IN, N_LAYER_OUT>("data/mnist_layer1_weights_small_784x256.dat", weights);
+//  rval = read_file_1D<bias_t, N_LAYER_OUT>("data/mnist_layer1_biases_small_256x1.dat", biases);
+//  rval = read_file_1D<float, N_LAYER_OUT>("data/validation_layer1_small_256x1.dat", answer);
 
   // Run the basic neural net block
   result_t res[N_LAYER_OUT];
-  ex_1layer(data, coeffs, biases, res);
+  ex_1layer(data, res);
 
   // Print result vector
   int err_cnt = 0;
