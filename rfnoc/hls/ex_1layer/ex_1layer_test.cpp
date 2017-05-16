c56570fc66d98a9573118cae1d561949df5e2358
ejk <ejkreinar@gmail.com>
2017-05-15 21:31:40 -0400

    1layer: minor change in testbench

--- rfnoc/hls/ex_1layer/ex_1layer_test.cpp
+++ rfnoc/hls/ex_1layer/ex_1layer_test.cpp
@@ -22,9 +22,9 @@ int main(int argc, char **argv)
 //  rval = read_file_2D<coeff_t, N_LAYER_IN, N_LAYER_OUT>("data/mnist_layer1_weights_784x10.dat", weights);
 //  rval = read_file_1D<bias_t, N_LAYER_OUT>("data/mnist_layer1_biases_10x1.dat", biases);
 //  rval = read_file_1D<input_t, N_LAYER_IN>("data/validation_data_784x1.dat", data);
+//  rval = read_file_1D<float, N_LAYER_OUT>("data/validation_layer1_small_256x1.dat", answer);
 //  rval = read_file_2D<coeff_t, N_LAYER_IN, N_LAYER_OUT>("data/mnist_layer1_weights_small_784x256.dat", weights);
 //  rval = read_file_1D<bias_t, N_LAYER_OUT>("data/mnist_layer1_biases_small_256x1.dat", biases);
-//  rval = read_file_1D<float, N_LAYER_OUT>("data/validation_layer1_small_256x1.dat", answer);
 
   // Run the basic neural net block
   result_t res[N_LAYER_OUT];
