177e927cf4a9a9c6ae1d3655940e779133391bfc
ejk <ejkreinar@gmail.com>
2017-05-16 22:47:40 -0400

    whitespace: Some tab to space conversion

--- rfnoc/hls/ex_1layer/ex_1layer.cpp
+++ rfnoc/hls/ex_1layer/ex_1layer.cpp
@@ -10,20 +10,25 @@
 // See UG902 Vivado High Level Synthesis guide (2014.4) pg 157 Figure 1-49
 //void nnet_basic (axis &in, axis &out) {
 void ex_1layer(
-	  input_t   data[N_LAYER_IN],
-	  result_t  res[N_LAYER_OUT],
-	  unsigned short &const_size_in,
-	  unsigned short &const_size_out)
+      input_t   data[N_LAYER_IN],
+      result_t  res[N_LAYER_OUT],
+      unsigned short &const_size_in,
+      unsigned short &const_size_out)
 {
     // Remove ap ctrl ports (ap_start, ap_ready, ap_idle, etc) since we only use the AXI-Stream ports
     #pragma HLS INTERFACE ap_ctrl_none port=return
 
-	// Connect size indicators
+    // Connect size indicators
     #pragma HLS INTERFACE ap_none port=const_size_in
     #pragma HLS INTERFACE ap_none port=const_size_out
-	const_size_in   = N_LAYER_IN;
-	const_size_out  = N_LAYER_OUT;
+    const_size_in   = N_LAYER_IN;
+    const_size_out  = N_LAYER_OUT;
 
-	nnet::layer<input_t, result_t, coeff_t, bias_t, accum_t> layer1;
-	layer1.compute<N_LAYER_IN, N_LAYER_OUT>(data, res, weights, biases);
+    // ****************************************
+    // NETWORK INSTATIATION
+    // ****************************************
+
+    // One fully connected layer
+    nnet::layer<input_t, result_t, coeff_t, bias_t, accum_t> layer1;
+    layer1.compute<N_LAYER_IN, N_LAYER_OUT>(data, res, weights, biases);
 }
