5df842dc0f76ab8e360848fb23f2c92278a16cb9
ejk <ejkreinar@gmail.com>
2017-07-05 19:19:56 -0400

    Use real weights for the modulation recognition network

--- rfnoc/hls/ex_modrec/ex_modrec.cpp
+++ rfnoc/hls/ex_modrec/ex_modrec.cpp
@@ -3,7 +3,19 @@
 #include "nnet_activation.h"
 
 #include "ex_modrec.h"
-#include "data/ex_modrec_weights.h"
+// #include "data/ex_modrec_weights.h"
+#include "data/ex_modrec_consts.h"
+
+template<class data_T, int N_IN>
+void  hls_stream_debug(hls::stream<data_T> &data, hls::stream<data_T> &res)
+{
+    data_T datareg;
+    for (int ii=0; ii<N_IN; ii++) {
+        datareg = data.read();
+        std::cout << "[" << ii << "]: " << datareg << std::endl;
+        res << datareg;
+    }
+}
 
 // AXI-Stream port type is compatible with pointer, reference, & array input / ouputs only
 // See UG902 Vivado High Level Synthesis guide (2014.4) pg 157 Figure 1-49
@@ -30,26 +42,24 @@ void ex_modrec(
 
     // LAYER 1
     hls::stream<layer1_t> logits1, hidden1;
-    nnet::compute_layer<input_t, layer1_t, weight_t, bias_t, accum_t, N_LAYER_IN, N_LAYER_1>(data, logits1, lay1_weights, lay1_biases);
+    nnet::compute_layer<input_t, layer1_t, weight_t, bias_t, accum_t, N_LAYER_IN, N_LAYER_1>(data, logits1, w1, b1);
     nnet::relu6<layer1_t, layer1_t, N_LAYER_1>(logits1, hidden1);
 
     // LAYER 2
     hls::stream<layer2_t> logits2, hidden2;
-    nnet::compute_layer<layer1_t, layer2_t, weight_t, bias_t, accum_t, N_LAYER_1, N_LAYER_2>(hidden1, logits2, lay2_weights, lay2_biases);
+    nnet::compute_layer<layer1_t, layer2_t, weight_t, bias_t, accum_t, N_LAYER_1, N_LAYER_2>(hidden1, logits2, w2, b2);
     nnet::relu6<layer2_t, layer2_t, N_LAYER_2>(logits2, hidden2);
 
     // LAYER 3
     hls::stream<layer3_t> logits3, hidden3;
-    nnet::compute_layer<layer2_t, layer3_t, weight_t, bias_t, accum_t, N_LAYER_2, N_LAYER_3>(hidden2, logits3, lay3_weights, lay3_biases);
+    nnet::compute_layer<layer2_t, layer3_t, weight_t, bias_t, accum_t, N_LAYER_2, N_LAYER_3>(hidden2, logits3, w3, b3);
     nnet::relu6<layer3_t, layer3_t, N_LAYER_3>(logits3, hidden3);
 
     // LAYER 4
     hls::stream<layer4_t> logits4, hidden4;
-    nnet::compute_layer<layer3_t, layer4_t, weight_t, bias_t, accum_t, N_LAYER_3, N_LAYER_4>(hidden3, logits4, lay4_weights, lay4_biases);
+    nnet::compute_layer<layer3_t, layer4_t, weight_t, bias_t, accum_t, N_LAYER_3, N_LAYER_4>(hidden3, logits4, w4, b4);
     nnet::relu6<layer4_t, layer4_t, N_LAYER_4>(logits4, hidden4);
 
     // LAYER 5
-    hls::stream<layer4_t> logits5, hidden5;
-    nnet::compute_layer<layer4_t, layer5_t, weight_t, bias_t, accum_t, N_LAYER_4, N_LAYER_5>(hidden4, logits5, lay5_weights, lay5_biases);
-    nnet::relu6<layer5_t, layer5_t, N_LAYER_5>(logits5, res);
+    nnet::compute_layer<layer4_t, layer5_t, weight_t, bias_t, accum5_t, N_LAYER_4, N_LAYER_5>(hidden4, res, w5, b5);
 }
--- rfnoc/hls/ex_modrec/ex_modrec_test.cpp
+++ rfnoc/hls/ex_modrec/ex_modrec_test.cpp
@@ -9,9 +9,16 @@ int main(int argc, char **argv)
 {
   // Modulation recognition examples
 
+  input_t   data[N_LAYER_IN];
+  float     answer[N_LAYER_OUT];
+
+  int rval = 0;
+  rval = read_file_1D<input_t, N_LAYER_IN>("data/ex_modrec_input_10x1.dat", data);
+  rval = read_file_1D<float, N_LAYER_OUT>("data/ex_modrec_output_10x1.dat", answer);
+
   hls::stream<input_t> data_strm;
-  for (int ii=0; ii < N_LAYER_IN; ii++) {
-    data_strm << ii;
+  for (int idat=0; idat < N_LAYER_IN; idat++) {
+    data_strm << data[idat];
   }
 
   hls::stream<result_t> res_strm;
@@ -26,10 +33,10 @@ int main(int argc, char **argv)
   float err, curr_data;
   for (int ii = 0; ii < N_LAYER_OUT; ii++) {
     curr_data = float(res_strm.read());
-    // err = float(res[ii])-answer[ii];
-    std::cout << "   Received: " << curr_data << std::endl;
-    // std::cout << " Expected: " << answer[ii] << "   Received: " << res[ii] << "  ErrVal: " << err << std::endl;
-    // if (abs(err) > 0.5) err_cnt++;
+    err = curr_data-answer[ii];
+    // std::cout << "   Received: " << curr_data << std::endl;
+    std::cout << " Expected: " << answer[ii] << "   Received: " << curr_data << "  ErrVal: " << err << std::endl;
+    if (abs(err) > 0.5) err_cnt++;
   }
   std::cout<< err_cnt << std::endl;
   return err_cnt;
