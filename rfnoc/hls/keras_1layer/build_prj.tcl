2eaf67abc1e04325b4c141d8087bff321dbc031a
EJ Kreinar <ejkreinar@case.edu>
2018-09-27 22:04:01 -0400

    keras_1layer: Create example based on hls4ml
    keras_1layer: Create Makefile and README

---
+++ rfnoc/hls/keras_1layer/build_prj.tcl
@@ -0,0 +1,46 @@
+#################
+#    HLS4ML
+#################
+array set opt {
+  csim   1
+  synth  1
+  cosim  1
+  export 1
+}
+
+foreach arg $::argv {
+  foreach o [lsort [array names opt]] {
+    regexp "$o +(\\w+)" $arg unused opt($o)
+  }
+}
+
+open_project -reset keras_1layer_prj
+set_top keras_1layer
+add_files firmware/keras_1layer.cpp -cflags "-I[file normalize ../../hls4ml/nnet_utils]"
+add_files -tb keras_1layer_test.cpp -cflags "-I[file normalize ../../hls4ml/nnet_utils]"
+add_files -tb firmware/weights
+#add_files -tb tb_data
+open_solution -reset "solution1"
+catch {config_array_partition -maximum_size 4096}
+set_part {xc7z020clg484-1}
+create_clock -period 5 -name default
+
+if {$opt(csim)} {
+  puts "***** C SIMULATION *****"
+  csim_design
+}
+
+if {$opt(synth)} {
+  puts "***** C/RTL SYNTHESIS *****"
+  csynth_design
+  if {$opt(cosim)} {
+    puts "***** C/RTL SIMULATION *****"
+    cosim_design -trace_level all
+  }
+  if {$opt(export)} {
+    puts "***** EXPORT IP *****"
+    export_design -format ip_catalog
+  }
+}
+
+exit
